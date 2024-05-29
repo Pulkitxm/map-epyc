d85e16d434a591d1bd0d1603aac96ce3395211fb
Pulkit <kpulkit15234@gmail.com>
2024-05-29 20:53:29 +0530

    search and shortlist people's popup

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -5,46 +5,39 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
-import LocalPeople from "@/data/people";
 
 function MapboxMap() {
   const [map, setMap] = useState<mapboxgl.Map>();
   const mapNode = useRef(null);
   const [people, setPeople] = useRecoilState(peopleAtom);
   const [markers, setMarkers] = useState<mapboxgl.Marker[]>([]);
-  const [done, setDone] = useState(false);
   useEffect(() => {
     const node = mapNode.current;
     if (typeof window === "undefined" || node === null) return;
-
+    
     const mapboxMap = new mapboxgl.Map({
       container: node,
       accessToken:
-        "pk.eyJ1IjoicHVsa2l0eG0iLCJhIjoiY2x3cm82NnQzMDJtajJrc2JwenJ5c2RrdiJ9.pb_qGeXbzAy-DHJnNMEDaA",
+      "pk.eyJ1IjoicHVsa2l0eG0iLCJhIjoiY2x3cm82NnQzMDJtajJrc2JwenJ5c2RrdiJ9.pb_qGeXbzAy-DHJnNMEDaA",
       center: [-74.5, 40],
-      zoom: 9,
+      // zoom: 9,
     });
     mapboxMap.addControl(new mapboxgl.NavigationControl(), "bottom-right");
     setMap(mapboxMap);
-
+    
     return () => {
       mapboxMap.remove();
     };
   }, []);
-
+  
   useEffect(() => {
-    if (!map || done) return;
+    if (!map) return;
 
-    // Remove all existing markers
-    markers.forEach((marker) => {
-      console.log(marker);
-      marker.remove();
-    });
-
+    // Remove old markers
+    markers.forEach((marker) => marker.remove());
+    
     // Add new markers
     const newMarkers = people.map((person) => {
-      person.fullName === "Abagael Priestley" && console.log(person);
-
       const newMarker = new mapboxgl.Marker()
         .setLngLat([
           person["location.lng"] as number,
@@ -52,7 +45,7 @@ function MapboxMap() {
         ])
         .setPopup(
           new mapboxgl.Popup({ closeOnClick: false }).setHTML(`
-            <div class="flex">
+            <div class="flex select-none">
               <img src="${person.photo}" class="h-[70px] rounded-full">
               <div class="flex flex-col justify-around">
                 <h1 class="text-lg">${person.fullName}</h1>
@@ -65,13 +58,8 @@ function MapboxMap() {
       newMarker.togglePopup();
       return newMarker;
     });
-    setDone(true);
     setMarkers(newMarkers);
   }, [map, people]);
-
-  useEffect(() => {
-    setPeople(LocalPeople);
-  }, [setPeople]);
 
   return <div ref={mapNode} style={{ width: "100vw", height: "100vh" }} />;
 }
--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -4,7 +4,10 @@ import algoliasearch from "algoliasearch/lite";
 import "instantsearch.css/themes/satellite.css";
 import { Hits, InstantSearch, SearchBox, Configure } from "react-instantsearch";
 import { Hit } from "./Hit";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { peopleAtom } from "@/globals/state/people";
+import LocalPeople from "@/data/people";
 
 const searchClient = algoliasearch(
   "4WWZ0EQ1ZQ",
@@ -13,6 +16,27 @@ const searchClient = algoliasearch(
 
 export default function Search() {
   const [inpValue, setInpValue] = useState<string | null>(null);
+  const setPeople = useSetRecoilState(peopleAtom);
+  useEffect(() => {
+    if (!inpValue) return;
+    const index = searchClient.initIndex("FrontendEngineer_Maps");
+    index
+      .search(inpValue, {
+        headers: {
+          "X-Algolia-Application-Id": "4WWZ0EQ1ZQ",
+          "X-Algolia-API-Key": "90048574fdaba4da82f82d5d9a0159e5",
+        },
+      })
+      .then(({ hits }) => {
+        setPeople(() => {
+          return LocalPeople.filter((person) =>
+            // @ts-expect-error - TS doesn't know about the id property
+            hits.some((hit) => hit.id === person.id)
+          );
+        });
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inpValue]);
   return (
     <div className="fixed z-50 top-5 left-5 ">
       <InstantSearch
